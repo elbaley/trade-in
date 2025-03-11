79e412d9b04a5638fc7db8fefe1f8640106da97e
elbaley <lebafurkan@outlook.com>
2025-03-11 14:11:23 +0300

    fix: demo account

--- server/src/auth/authMiddleware.ts
+++ server/src/auth/authMiddleware.ts
@@ -6,6 +6,12 @@ export const authMiddleware = async (c: Context<HonoEnv, "*">, next: Next) => {
   if (!user) {
     return c.json({ status: false, message: "Unauthorized" }, 401);
   }
+  if (user.email === "demo@furkanleba.com" && c.req.method !== "GET") {
+    return c.json(
+      { status: false, message: "Demo user can't perform this action" },
+      401,
+    );
+  }
 
   return next();
 };
